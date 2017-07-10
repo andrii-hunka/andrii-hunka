325a9e9af9c2acf9b45f453bddd9b8155d597ba3
andrii-hunka <andriyggg@gmail.com>
2017-07-10 15:11:08 +0300

    fix

--- 09_js-intro/js/task2.js
+++ 09_js-intro/js/task2.js
@@ -1,6 +1,6 @@
 var usd = 25.8,
     eur = 29.1,
-    usdQuantity = prompt("Кількість євро"),
-    eurQuantity = prompt("Кількість доларів");
+    eurQuantity = prompt("Кількість євро"),
+    usdQuantity = prompt("Кількість доларів");
 console.log(`${eurQuantity} euros are equal ${eurQuantity*eur} UAH, ${usdQuantity} dollars are equal ${usdQuantity*usd} UAH,one euro is equal ${(eur/usd).toFixed(3)} dollars.`);
     
