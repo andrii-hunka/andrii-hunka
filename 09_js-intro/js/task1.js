dbf108eca2aece8c18a61395d1bc12abd3402250
andrii-hunka <andriyggg@gmail.com>
2017-07-11 10:58:26 +0300

    fix

--- 09_js-intro/js/task1.js
+++ 09_js-intro/js/task1.js
@@ -4,5 +4,5 @@ var a = prompt("Insert a"),
     d = Math.pow(b,2) - 4*a*c;
     res1 = (-b + Math.sqrt(d))/(2*a),
     res2 = (-b - Math.sqrt(d))/(2*a);
-console.log(`Рівняння ax2 + bx + c = 0 має 2 розв’язки: x1 = ${res1}, x2 = ${res2}`);
+console.log(`Рівняння ${a}x2 + ${b}x + ${c} = 0 має 2 розв’язки: x1 = ${res1}, x2 = ${res2}`);
 
